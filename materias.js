eab1525b79e76e234dca1d9cc52e15478394e9c6
Mario Valverde <mv5029@live.com>
2021-07-27 22:12:57 -0500

    crea archivo materias

--- materias.js
+++ materias.js
@@ -1,3 +1,116 @@
+diaNum = {
+	lunes: 0,
+	martes: 1,
+	miercoles: 2,
+	jueves: 3,
+	viernes: 4,
+	sabado: 5,
+	domingo: 6
+}
+
+numDia = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo']
+
 materias = [
-	{ nombre: '' }
+	{
+		nombre: 'Biofisica',
+		dias: {
+			martes: {
+				inicio: "12:00",
+				termina: "14:00"
+			},
+			miercoles: {
+				inicio: "8:00",
+				termina: "10:00"
+			},
+			jueves: {
+				inicio: "8:00",
+				termina: "10:00"
+			}
+		}
+	},
+	{
+		nombre: 'Computacion III',
+		dias: {
+			lunes: {
+				inicio: "12:00",
+				termina: "13:00"
+			},
+			martes: {
+				inicio: "10:00",
+				termina: "12:00"
+			},
+			jueves: {
+				inicio: "12:00",
+				termina: "14:00"
+			}
+		}
+	},
+	{
+		nombre: 'Estencia V',
+		dias: {
+			martes: {
+				inicio: "17:00",
+				termina: "19:00"
+			},
+			jueves: {
+				inicio: "17:00",
+				termina: "19:00"
+			}
+		}
+	},
+	{
+		nombre: 'Fisica Moderna',
+		dias: {
+			lunes: {
+				inicio: "13:00",
+				termina: "15:00"
+			},
+			martes: {
+				inicio: "8:00",
+				termina: "10:00"
+			},
+			viernes: {
+				inicio: "8:00",
+				termina: "10:00"
+			}
+		}
+	},
+	{
+		nombre: 'Microcontroladores',
+		dias: {
+			lunes: {
+				inicio: "16:00",
+				termina: "17:30"
+			},
+			miercoles: {
+				inicio: "12:00",
+				termina: "14:00"
+			},
+			jueves: {
+				inicio: "10:00",
+				termina: "12:00"
+			},
+			viernes: {
+				inicio: "10:00",
+				termina: "11:30"
+			}
+		}
+	},
+	{
+		nombre: 'Estadistica',
+		dias: {
+			lunes: {
+				inicio: "10:00",
+				termina: "12:00"
+			},
+			miercoles: {
+				inicio: "10:00",
+				termina: "12:00"
+			}
+		}
+	}
 ]
+
+exports.materias = materias
+exports.diaNum = diaNum
+exports.numDia = numDia
