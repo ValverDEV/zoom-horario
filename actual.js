a4dd1ba352984ff6c83f893956065caee305ff01
Mario Valverde <mv5029@live.com>
2021-08-11 13:26:35 -0500

    obtiene link micro desde aulas-unam

--- actual.js
+++ actual.js
@@ -3,7 +3,7 @@ const estadistica2 = 'https://cuaieed-unam.zoom.us/j/85252440658'
 const computacion = 'https://cuaieed-unam.zoom.us/j/84692670727'
 const fisica = 'https://cuaieed-unam.zoom.us/j/88904812467?pwd=amd0WC9Zdi94TGJRaDVPVXFwaUFrQT09'
 const fisicaLab = 'https://cuaieed-unam.zoom.us/j/83516684281'
-const micro = 'https://aulas-virtuales.cuaieed.unam.mx/alumnos.html'
+const micro = 'DOMINGO RANGEL MIRANDA'
 const estancia = 'https://us02web.zoom.us/j/87055264048?pwd=VjBsMG82TEpiY21PMU9UVlZGZlM5Zz09'
 const biofisica = 'https://us02web.zoom.us/j/88482667789?pwd=aG85WCtuZWMySXEyNW9qNW0rc29FUT09'
 
@@ -33,7 +33,7 @@ const clases = [
 			url: fisica
 		},
 		{
-			nombre: 'Microcontroladores 🤡🤡🤡',
+			nombre: 'Microcontroladores',
 			inicio: '16:00',
 			termina: '17:30',
 			url: micro
@@ -81,7 +81,7 @@ const clases = [
 			url: estadistica2
 		},
 		{
-			nombre: 'Microcontroladores 🤡🤡🤡',
+			nombre: 'Microcontroladores',
 			inicio: '12:00',
 			termina: '14:00',
 			url: micro
@@ -96,7 +96,7 @@ const clases = [
 			url: biofisica
 		},
 		{
-			nombre: 'Microcontroladores 🤡🤡🤡',
+			nombre: 'Microcontroladores',
 			inicio: '10:00',
 			termina: '12:00',
 			url: micro
@@ -123,7 +123,7 @@ const clases = [
 			url: fisicaLab
 		},
 		{
-			nombre: 'Microcontroladores 🤡🤡🤡',
+			nombre: 'Microcontroladores',
 			inicio: '10:00',
 			termina: '11:30',
 			url: micro
@@ -145,11 +145,33 @@ const launchCurrent = function (nombre, url) {
 	}, 5000)
 }
 
+const encontrar = function (res) {
+	let data = res.data
+	console.log(data)
+	for (let obj of data) {
+		if (obj.host == micro) {
+			let url = 'https://cuaieed-unam.zoom.us/j/'
+			url += obj.id
+			launchCurrent('Microcontroladores', url)
+		}
+	}
+}
+
+const casoEspecial = function () {
+	axios.get('https://aulas-virtuales.cuaieed.unam.mx:3000/api/zoom_live_meetings')
+		.then(encontrar)
+		.catch(function (error) { h1.innerText = 'Error al encontrar clase de Microcontroladores :(' })
+}
+
 const claseActual = function (clasesDia, currentMins) {
 	for (let clase of clasesDia) {
 		const inicio = hourToMins(clase.inicio)
 		const termina = hourToMins(clase.termina)
 		if (inicio - 5 < currentMins && termina - 5 > currentMins) {
+			if (clase.nombre == 'Microcontroladores') {
+				casoEspecial()
+				return true
+			}
 			launchCurrent(clase.nombre, clase.url)
 			return true
 		}
